cb7548adc630d6dc8a3e5bcdf32aba5296e55228
Jonathan MirCha <jonmircha@gmail.com>
2023-10-19 10:38:39 -0600

    Maquetación con Bootstrap

--- FundamentosWeb/js/intro-bootstrap.js
+++ FundamentosWeb/js/intro-bootstrap.js
@@ -1,1 +1,10 @@
 console.log("Hola Mundo con Bootstrap");
+
+/* 
+URLs de Video AMERIKE
+
+- https://amerike.edu.mx/wp-content/uploads/2023/09/licmusicalamerike_720-720p.mp4
+- https://amerike.edu.mx/wp-content/uploads/2023/09/licaudiovisual_desk.mp4
+- https://amerike.edu.mx/wp-content/uploads/2023/09/ingvideojuegos_desk.mp4
+
+*/
